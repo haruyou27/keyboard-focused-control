1fd1ad6190ecc52d8f32c27fe96e34a5ac4f8a3b
Haruyou27 <emaildai27@gmail.com>
2022-10-31 23:39:18 +0700

    got it to work

--- InspectorInterface.gd
+++ InspectorInterface.gd
@@ -1,6 +1,7 @@
 extends EditorInspectorPlugin
 
 var box := preload("res://addons/keyboard-focused-control/box.gd")
+var grid := preload("res://addons/keyboard-focused-control/grid.gd")
 
 func can_handle(object):
 	return object is BoxContainer or object is GridContainer
@@ -11,6 +12,7 @@ func parse_category(object, category):
 		
 	if object is BoxContainer:
 		add_property_editor('', box.new())
+		
 	elif object is GridContainer:
-		pass
+		add_property_editor('', grid.new())
 	
--- box.gd
+++ box.gd
@@ -6,6 +6,7 @@ func _init():
 	label = 'Assign focus neighbour'
 	button.text = 'Run'
 	button.connect("pressed", self, '_assign')
+	
 	add_child(button)
 	add_focusable(button)
 	
@@ -23,8 +24,8 @@ func _assign():
 	i = 0
 	var size := nodes.size()
 	for node in nodes:
-		node.focus_neighbour_left = nodes[i-1].get_path()
-		node.focus_neighbour_right = nodes[0-size+i+1].get_path()
+		node.focus_neighbour_left = '../' + nodes[i-1].name
+		node.focus_neighbour_right = '../' + nodes[0-size+i+1].name
 		
 		node.focus_neighbour_top = node.focus_neighbour_left
 		node.focus_neighbour_bottom = node.focus_neighbour_right
